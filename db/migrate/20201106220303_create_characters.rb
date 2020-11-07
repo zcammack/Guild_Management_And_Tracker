dedd96b28bf278c997240ae369f39b85330ee567
zcammack <zack.cammack@gmail.com>
2020-11-07 01:22:49 -0600

    Followed a short guide into more understanding with Devise. We now have a root page with a sign in and the beginning of a navigation bar.

--- db/migrate/20201106220303_create_characters.rb
+++ db/migrate/20201106220303_create_characters.rb
@@ -2,11 +2,18 @@ class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :race
-      t.string :rank
+      t.integer :age
       t.string :class
-      t.string :alias
-
+      t.string :race
+      t.integer :level
+      t.integer :strength
+      t.integer :dexterity
+      t.integer :constitution
+      t.integer :intelligence
+      t.integer :wisdom
+      t.integer :charisma
+      t.integer :user_id
+      t.integer :campaign_id
       t.timestamps
     end
   end
--- db/migrate/20201107065036_create_campaigns.rb
+++ db/migrate/20201107065036_create_campaigns.rb
@@ -1,6 +1,12 @@
 class CreateCampaigns < ActiveRecord::Migration[6.0]
   def change
     create_table :campaigns do |t|
+      t.string :title
+      t.string :universe_setting
+      t.string :dungeonmaster
+      t.boolean :available
+      t.datetime :startdate
+      t.datetime :nextsession
 
       t.timestamps
     end
