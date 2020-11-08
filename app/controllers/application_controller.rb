3dd64ffe323d06f6e49b2b230a03b0e8b4919799
zcammack <zack.cammack@gmail.com>
2020-11-07 23:07:41 -0600

    Added authentication from devise to applicationcontroller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!, :except => [:welcome]
+
+    def welcome
+
+    end
+
+    def current_user
+        @user = current_user
+    end
+
 end
--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-    before_action :authenticate_user!
-
-    user_signed_in?
-
-    current_user
-
-    user_session
-end
