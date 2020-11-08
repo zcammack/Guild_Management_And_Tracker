ef3545686596c258282957a34ae2f3092b29eb14
zcammack <zack.cammack@gmail.com>
2020-11-07 22:56:20 -0600

    Created some validations for the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
-  has_many :characters
-  has_many :campaigns, through: :characters
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  has_many :characters
+  has_many :campaigns, through: :characters
+
+  validates_presence_of :name, uniqueness: true
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, uniqueness: true
 end
