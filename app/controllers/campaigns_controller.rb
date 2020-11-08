f6df6b85e4267a797de49501693ed988fc40bc3e
zcammack <zack.cammack@gmail.com>
2020-11-08 11:11:14 -0600

    Created new and create actions for campaigns.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,7 +2,31 @@ class CampaignsController < ApplicationController
 
     before action :set_campaign, only: [:show, :edit, :update, :destroy]
 
+    def index
+        if user_signed_in?
+            @campaigns = current_user.campaigns
+        else
+            flash[:alert = "Please sign up or login to see your campaigns."]
+            redirect_to new_user_session_path
+        end
+    end
 
+    def new
+        @campaign = Campaign.new
+        5.times{@campaign.characters.build(user:current_user)}
+    end
+
+    def create
+        @campaign = current_user.traveler.campaign.build(campaign_params)
+        if @campaign.save
+            flash[:notice] = "Campaign successfully created."
+            redirect to campaigns_path
+        else
+            @errors = @campaign.errors.full_messages
+            flash[:alert] = "Campaign was not created. Please try again."
+            render :'/campaigns/new'
+        end
+    end
 
     private
 
