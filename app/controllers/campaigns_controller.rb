124710485190d0263983b2d58ab7d0c18acc42fd
zcammack <zack.cammack@gmail.com>
2020-11-08 11:03:55 -0600

    Created some useful helpers, similar to the characters controller.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,16 @@
 class CampaignsController < ApplicationController
+
+    before action :set_campaign, only: [:show, :edit, :update, :destroy]
+
+
+
+    private
+
+    def campaign_params
+        params.require(:campaign).permit(:title, :universe_setting, :dungeonmaster, :available)
+    end
+
+    def set_campaign
+        @campaign = Campaign.find(params[:id])
+    end
 end
