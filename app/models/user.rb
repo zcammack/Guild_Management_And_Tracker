082d78da1d040bd35d5d6e4309d6301f9bd00293
zcammack <zack.cammack@gmail.com>
2020-11-07 23:02:18 -0600

    Put together an omni-auth method for use in the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@ class User < ApplicationRecord
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :email, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.name = auth.info.name
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
