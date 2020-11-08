d46445f2e5c476f49d84fdcaa2fabb43a52b37c5
zcammack <zack.cammack@gmail.com>
2020-11-08 11:21:53 -0600

    Added nested attributes for characters into campaign.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,10 @@
 class Campaign < ApplicationRecord
-    has_many :characters
+    has_many :characters, :dependent => :destroy
     has_many :users, through: :characters
+
+    accepts_nested_attributes_for :characters, reject_if: proc {|attributes| attributes["name"].blank?}
+
+    validates :name, presence: :true
+    validates :dungeonmaster, presence: :true
+
 end
