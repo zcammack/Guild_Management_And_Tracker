72c514237a3b640b51ed171ff55dec9492301f23
zcammack <zack.cammack@gmail.com>
2020-11-07 22:49:37 -0600

    Fixed weird error from missing gem in gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
+
+    user_signed_in?
+
+    current_user
+
+    user_session
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   resources :campaigns
   resources :characters
   devise_for :admins
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'campaign#index'
 end
