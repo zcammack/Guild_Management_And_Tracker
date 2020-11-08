339f11c0e98a2c255d0db43e5fa4abd203df49a7
zcammack <zack.cammack@gmail.com>
2020-11-07 23:13:58 -0600

    Started with character controller next. Created a few helpers to start with.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,19 @@
 class CharactersController < ApplicationController
+    before_action :set_campaign, only: [:index, :new, :create, :show]
+
+
+
+
+
+
+
+    private
+
+    def character_params
+        params.require(:character).permit(:name, :age, :class, :race, :level, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :campaign_id, :user_id)
+    end
+
+    def set_campaign
+        @campaign = Campaign.find_by(id:params[:campaign_id])
+    end
 end
